0b82c0d44d0e3d141ef440835784171fb9d55959
root <root@DESKTOP-2QCHDLB.localdomain>
2022-04-12 19:03:17 -0500

    chapter2

---
+++ views/employee_requests.py
@@ -0,0 +1,30 @@
+EMPLOYEES = [
+    {
+      "id": 1,
+      "name": "Denise",
+      "locationId": 1
+    },
+    {
+      "id": 2,
+      "name": "Jacky",
+      "locationId": 3
+    },
+    {
+      "id": 3,
+      "name": "Nick",
+      "locationId": 2
+    },
+    {
+      "id": 4,
+      "name": "Jason",
+      "locationId": 1
+    },
+    {
+      "id": 5,
+      "name": "DJ",
+      "locationId": 2
+    }
+]
+
+def get_all_employees():
+    return EMPLOYEES
---
+++ views/location_requests.py
@@ -0,0 +1,20 @@
+LOCATIONS = [
+    {
+      "id": 1,
+      "name": "Nashville North ",
+      "address": "8422 Johnson Pike E"
+    },
+    {
+      "id": 2,
+      "name": "Nashville South",
+      "address": "209 Emory Drive"
+    },
+    {
+      "id": 3,
+      "name": "Nashville",
+      "address": "100 Copper Drive"
+    }
+]
+
+def get_all_locations():
+    return LOCATIONS
